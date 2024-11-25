7f1203fdc9cd6b890f0e9841f55c5585586aeb27
KANTAPHAT LEELAKUNWET521370990017 <kantaphat.lee@sjtu.edu.cn>
2024-11-25 15:19:13 +0800

    feat: initialize neuralODE for cbf

--- dex/modules/samplers.py
+++ dex/modules/samplers.py
@@ -1,9 +1,15 @@
 from ..utils.general_utils import AttrDict, listdict2dictlist
 from ..utils.rl_utils import ReplayCache
+from .cbf import ODEFunc
+
+import os
+import torch
+import PIL.Image as Image
 
 
 class Sampler:
     """Collects rollouts from the environment using the given agent."""
+
     def __init__(self, env, agent, max_episode_len):
         self._env = env
         self._agent = agent
@@ -13,23 +19,35 @@ class Sampler:
         self._episode_step = 0
         self._episode_cache = ReplayCache(max_episode_len)
 
+        self.device = torch.device(
+            'cuda:' + str(0)
+            if torch.cuda.is_available() else 'cpu'
+        )
+
+        # Initialize neuralODE for CBF (Evaluation ONLY)
+        self.func = ODEFunc([3, 64, 12]).to(self.device)
+        self.func.load_state_dict(torch.load("./dex/modules/model_test10.pth"))
+        self.func.eval()
+
     def init(self):
         """Starts a new rollout. Render indicates whether output should contain image."""
         self._episode_reset()
 
     def sample_action(self, obs, is_train):
         return self._agent.get_action(obs, noise=is_train)
-    
+
     def sample_episode(self, is_train, render=False, random_act=False):
         """Samples one episode from the environment."""
         self.init()
         episode, done = [], False
         while not done and self._episode_step < self._max_episode_len:
-            action = self._env.action_space.sample() if random_act else self.sample_action(self._obs, is_train) 
+            action = self._env.action_space.sample(
+            ) if random_act else self.sample_action(self._obs, is_train)
             if action is None:
                 break
             if render:
                 render_obs = self._env.render('rgb_array')
+
             obs, reward, done, info = self._env.step(action)
             episode.append(AttrDict(
                 reward=reward,
@@ -44,7 +62,8 @@ class Sampler:
             self._obs = obs
             self._episode_step += 1
 
-        episode[-1].done = True     # make sure episode is marked as done at final time step
+        # make sure episode is marked as done at final time step
+        episode[-1].done = True
         rollouts = self._episode_cache.pop()
         assert self._episode_step == self._max_episode_len
         return listdict2dictlist(episode), rollouts, self._episode_step
