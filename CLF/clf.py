9ef058a207c02887c13d7dd0a4c68e2229a6c3ac
KANTAPHAT LEELAKUNWET521370990017 <kantaphat.lee@sjtu.edu.cn>
2024-12-22 15:11:05 +0800

    feat: implement CLF (incomplete)

---
+++ CLF/clf.py
@@ -0,0 +1,133 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+from cvxopt import solvers, matrix
+
+
+def cvx_solver(P, q, G, h):
+    mat_P = matrix(P.cpu().numpy())
+    mat_q = matrix(q.cpu().numpy())
+    mat_G = matrix(G.cpu().numpy())
+    mat_h = matrix(h.cpu().numpy())
+
+    solvers.options['show_progress'] = False
+
+    sol = solvers.qp(mat_P, mat_q, mat_G, mat_h)
+
+    return sol['x']
+
+
+class ODEFunc(nn.Module):
+
+    def __init__(self, fc_param):
+        super(ODEFunc, self).__init__()
+
+        self.net = self.build_mlp(fc_param)
+
+        # Initializing weights
+        for m in self.net.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, mean=0, std=0.1)
+                nn.init.constant_(m.bias, val=0)
+
+        self.u = None
+        self.device = torch.device(
+            'cuda:' + str(0)
+            if torch.cuda.is_available() else 'cpu'
+        )
+
+    def forward(self, t, x):
+        # x.shape = [20, 1, 3]
+        if self.training:
+
+            net_out = self.net(x)  # [20, 1, 6]
+
+            # \dot{x} = f(x) + g(x) * u
+            fx = net_out[:, :, :3]  # [20, 1, 3]
+            gx = net_out[:, :, 3:]  # [20, 1, 3]
+
+            u = self.u  # [20, 1, 1]
+
+            out = fx + gx * u  # [20, 1, 3]
+
+        else:
+            # For test and evaluation
+
+            net_out = self.net(x)  # [1, 6]
+
+            # \dot{x} = f(x) + g(x) * u
+            fx = net_out[:, :3]  # [1, 3]
+            gx = net_out[:, 3:]  # [1, 3]
+
+            u = self.u  # [1, 1]
+
+            out = fx + gx * u  # [1, 3]
+
+        return out
+
+    def dCLF(self, robot, gt, u, f, g):
+        """
+        Enforce CLF on action
+
+        Args:
+            robot  ([1, 3]): robot orientation
+            gt ([1, 3]): orientation from expert trajectory
+            u  ([1, 1]): action
+            f  ([1, 3]): fx
+            g  ([1, 3]): gx
+        """
+        # Assign robot orientation
+        x, y, z = robot[0, 0], robot[0, 1], robot[0, 2]
+
+        # Expert Demonstration's Orientation
+        x0, y0, z0 = gt[0, 0], gt[0, 1], gt[0, 2]
+
+        # Compute CLF
+        V = (x - x0) ** 2 + (y - y0) ** 2 + (z - z0) ** 2
+
+        dotV = 2 * (x - x0) * (f[0, 0] + g[0, 0] * u[0, 0]) \
+            + 2 * (y - y0) * (f[0, 1] + g[0, 1] * u[0, 0]) \
+            + 2 * (z - z0) * (f[0, 2] + g[0, 2] * u[0, 0])
+
+        gamma = 1
+        # dotV + epsilon * V <= 0
+        b_safe = gamma * V
+        A_safe = -dotV
+
+        dim = g1.shape[1]  # = 3
+        G = A_safe.to(self.device)
+        h = b_safe.unsqueeze(0).to(self.device)  # [1, 1]?
+        P = torch.eye(dim).to(self.device)  # [3, 3]
+        q = -u.T  # [3, 1]
+
+        # NOTE: different x from above now
+        x = cvx_solver(P.double(), q.double(), G.double(), h.double())
+
+        out = []
+        for i in range(dim):
+            out.append(x[i])
+        out = np.array(out)
+        out = torch.tensor(out).float().to(self.device)
+        out = out.unsqueeze(0)
+        return out
+
+    def build_mlp(self, filters, no_act_last_layer=True, activation='gelu'):
+        if activation == 'gelu':
+            activation = nn.GELU()
+        elif activation == 'silu':
+            activation = nn.SiLU()
+        elif activation == 'tanh':
+            activation = nn.Tanh()
+        else:
+            raise NotImplementedError(
+                f'Not supported activation function {activation}')
+        modules = nn.ModuleList()
+        for i in range(len(filters)-1):
+            modules.append(nn.Linear(filters[i], filters[i+1]))
+            if not (no_act_last_layer and i == len(filters)-2):
+                modules.append(activation)
+
+        modules = nn.Sequential(*modules)
+        return modules
---
+++ CLF/eval.py
@@ -0,0 +1,107 @@
+import os
+import argparse
+import numpy as np
+import torch
+from torchdiffeq import odeint
+from clf import ODEFunc
+
+parser = argparse.ArgumentParser('ODE demo')
+parser.add_argument('--method', type=str,
+                    choices=['dopri5', 'adams'], default='dopri5')
+parser.add_argument('--data_size', type=int, default=50)
+parser.add_argument('--gpu', type=int, default=0)
+parser.add_argument('--use_dclf', action='store_true')
+
+args = parser.parse_args()
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+device = torch.device(
+    'cuda:' + str(args.gpu)
+    if torch.cuda.is_available() else 'cpu'
+)
+
+# Load dataset
+obs = np.load('../CBF/data/NeedlePick-v0/obs_orn.npy')  # [100, 51, 4]
+obs = obs[:, :, 0:3]  # [100, 51, 3]
+obs = torch.tensor(obs).float()
+SCALING = 5.0
+acs = np.load('../CBF/data/NeedlePick-v0/acs_orn.npy')  # [100, 50 ,2]
+acs = acs[:, :, 0] * np.deg2rad(30)  # [100, 50, 1]
+acs = torch.tensor(acs).float()
+
+# Training data
+x_train = obs.unsqueeze(2).to(device)  # [100, 51, 1, 3]
+u_train = acs.unsqueeze(2).to(device)  # [100, 50, 1, 3]
+
+# Testing data
+x_test = x_train[-1, :, :, :]  # [51, 1, 3]
+u_test = u_train[-1, :, :, :]  # [50, 1, 3]
+
+# Initial condition for testing
+x_test0 = x_train[-1, 0, :, :]  # [1, 3]
+u_test0 = u_train[-1, 0, :, :]  # [1, 3]
+
+# Set up the dimension of the network
+x_dim = x_train.shape[-1]
+u_dim = u_train.shape[-1]
+fc_param = [x_dim, 64, x_dim + x_dim * u_dim]
+
+# Initialize neural ODE
+func = ODEFunc(fc_param).to(device)
+func.load_state_dict(torch.load(
+    "../CBF/saved_model/{args.task}_{exp_id}/CLF10.pth"))
+func.eval()
+
+# Set up initial state
+tt = torch.tensor([0., 0.1]).to(device)
+x0 = x_test0.clone().detach().to(device)
+pred_x = x0.unsqueeze(0)
+
+safety = []
+trajectory = []
+trajectory.append(
+    np.array(
+        [x0[0, 0].cpu().item(), x0[0, 1].cpu().item(), x0[0, 2].cpu().item()]
+    )
+)
+
+with torch.no_grad():
+
+    for i in range(args.data_size):
+        # Setup u for forward()
+        # u_test[i,:,:] is the actions of test trajectory that at time i
+        u_test_i = u_test[i, :, :]  # [1, 1]
+
+        if args.use_dcbf:
+
+            net_out = func.net(x0)  # [1, 6]
+
+            # \dot{x} = f(x) + g(x) * u
+            fx = net_out[:, :3]  # [1, 3]
+            gx = net_out[:, 3:]  # [1, 3]
+
+            func.u = func.dCLF(x0, x_test[i, :, :], u_test_i, fx, gx)
+        else:
+            func.u = u_test_i
+
+        pred = odeint(func, x0, tt)
+        pred_x = torch.cat(
+            [pred_x, pred[-1, :, :].unsqueeze(0)], dim=0)
+        x0 = pred[-1, :, :]
+        trajectory.append(
+            np.array(
+                [x0[0, 0].cpu().item(), x0[0, 1].cpu().item(), x0[0, 2].cpu().item()]
+            )
+        )
+
+        # Compute the distance between robot and obstacle point
+        barrier = (x0[0, 0] - 2.67054296) ** 2 \
+            + (x0[0, 1] - (-0.04659402)) ** 2  \
+            + (x0[0, 2] - 3.4671967) ** 2     \
+            - 0.05 ** 2
+        safety.append(barrier)
+
+
+print("====== Safety ======")
+print(torch.sum(torch.tensor(safety) < 0))
+print(np.array(trajectory))
